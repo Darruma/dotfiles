21ce3dffb6b1c9549729d77a82dffce904b6ff8d
darruma <umerderr@gmail.com>
2019-01-21 15:51:39 +0000

    use find to symlink directories

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,10 @@
-
 echo "Installing dotfiles"
-
 echo "Proceed (y/n)"
-
 read ans
-
-if [ "$ans" = 'y'] ; then
-        for dotfile in $(ls -a)); do
-                ln -sv "$PWD/$dotfile" "$HOME"
+if [ $ans = 'y' ] 
+then
+        for dotfile in $(find -type f | grep -vf  .gitignore | grep -v .gitignore); do
+                ln -sfv "$PWD/$dotfile" "$HOME/$dotfile"
         done
         echo "Symlink complete"
 else
