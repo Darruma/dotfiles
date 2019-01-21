d299db6f475318fa6f526d4bd1ac1e0f2edfd3fa
darruma <umerderr@gmail.com>
2019-01-21 16:00:41 +0000

    refactored with functions

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,12 +1,26 @@
-echo "Installing dotfiles"
-echo "Proceed (y/n)"
-read ans
-if [ $ans = 'y' ] 
-then
+
+function dotfiles()
+{
+    echo "Installing dotfiles"
+    echo "Proceed (y/n)"
+    read ans
+    if [ $ans = 'y' ] 
+    then
         for dotfile in $(find -type f | grep -vf  .gitignore | grep -v .gitignore); do
                 ln -sfv "$PWD/$dotfile" "$HOME/$dotfile"
         done
         echo "Symlink complete"
-else
+    else
         echo "Symlink cancelled"        
-fi
+    fi
+}
+
+function progs()
+{
+ pacman -Syu ttf-inconsolata powerline powerline-fonts ranger w3m feh git
+
+}
+
+
+dotfiles
+progs 
